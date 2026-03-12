aad539df82e99c424af6904c2fd978b3b3a84c84
Dale Santos <cdalesantos9@gmail.com>
2026-03-12 09:47:28 +0800

    Revert "Update AIInsights.js"
    This reverts commit ee10db4e95102e81fdf5dcb66bc91874063c4461.

--- frontend/src/components/AIInsights.js
+++ frontend/src/components/AIInsights.js
@@ -1,7 +1,7 @@
 import React, { useState, useMemo } from 'react';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || "";
+const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
 
 export default function AIInsights({ transactions }) {
   const [insight, setInsight] = useState("");
